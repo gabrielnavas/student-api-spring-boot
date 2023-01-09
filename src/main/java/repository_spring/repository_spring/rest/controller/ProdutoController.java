d5dd6dbd4a7f3f93a9e5ee08f5ef0f6d486296ce
navas <gabnavas.dev@gmail.com>
2023-01-09 14:30:11 -0300

    create product controller

---
+++ src/main/java/repository_spring/repository_spring/rest/controller/ProdutoController.java
@@ -0,0 +1,39 @@
+package repository_spring.repository_spring.rest.controller;
+
+import javax.websocket.server.PathParam;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import repository_spring.repository_spring.domain.entity.Produto;
+import repository_spring.repository_spring.domain.repository.Produtos;
+
+@RestController()
+@RequestMapping("/api/produtos")
+public class ProdutoController {
+
+  @Autowired
+  private Produtos produtos;
+
+  @PostMapping()
+  @ResponseStatus(HttpStatus.CREATED)
+  public Produto save(@RequestBody Produto produto) {
+    try {
+      return produtos.save(produto);
+    } catch(Exception ex) {
+      if(ex instanceof ResponseStatusException) {
+        throw ex;
+      }
+      System.out.println(ex); // utilizar um logger depois
+      throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "server error");
+    }
+  }
+}
